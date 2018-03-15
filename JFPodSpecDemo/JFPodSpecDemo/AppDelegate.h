f3e8e39a02f6d9cce7d240d3a5685b06ea2b8df1
杜金锋 <dujinfeng@meituan.com>
2018-03-15 10:39:42 +0800

    init proj

---
+++ JFPodSpecDemo/JFPodSpecDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFPodSpecDemo
+//
+//  Created by adler on 2018/3/15.
+//  Copyright © 2018年 adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFPodSpecDemo/JFPodSpecDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JFPodSpecDemo
+//
+//  Created by adler on 2018/3/15.
+//  Copyright © 2018年 adler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
